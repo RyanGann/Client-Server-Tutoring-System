928cbd72281608dcfeab0a29dce5c60068c0adfc
Mary831 <82053519+Mary831@users.noreply.github.com>
2021-04-06 14:13:21 -0500

    Add files via upload

--- student.py
+++ student.py
@@ -1,24 +1,61 @@
 #list of commands
 
+
 import sys
 import os
+#import version1
 
-import json
+def stuff():   #note change this name
+    cmd = ""
+    while cmd != "exit":
+        cmd = (input('> '))
+        #version1.clear_screen()
+        if cmd == "regact":
+            regact()
+        elif cmd == "edact":   #this was not on overleaf doc
+            editacct()
+        elif cmd == "vtut":
+            vtut()
+        elif cmd == "aapt":
+            aapt()
+        elif cmd == "edapt":
+            edapt()
+        elif cmd == "dapt":
+            dapt()
+        elif cmd == "emcon":
+            emcon()
+        elif cmd == "emrem":
+            emrem()
+        elif cmd == "psr":
+            psr()
+        else:
+            print("invalid command")
 
 
-def mkapt(date, subject, tutor):
-    data = {}
-    data['appointment'] = []
-    data['appointment'].append({
-        'date': date,
-        'subject': subject,
-        'tutor': tutor
-    })
 
-    with open('newApt.txt', 'w') as outfile:
-        json.dump(data, outfile)
-        
-    print("Appointment has been made and scheduled")
+def regact():
+    print("register account")
 
-def resetpswrd():
+def edact():
+    print("edit account")
+
+def viewtutors():
+    print("view tutors")
+
+def aapt():
+    print("Appointment has been added and scheduled")
+
+def edapt():
+    print("edit appointment")
+
+def dapt():
+    print("delete appointment")
+
+def emcon():
+    print("email confirmation")
+
+def emrem():
+    print("email reminder")
+
+def psr():
     print("Password has been reset")
