84e8525cd4523992d8a5cf7f12839c1682c231c9
Ryan Gann <43328613+RyanGann@users.noreply.github.com>
2021-04-07 17:52:58 -0500

    Add files via upload

---
+++ tutorSched.py
@@ -0,0 +1,10 @@
+import appointment
+
+class tutorSched():
+    
+    def __init__(newCourse, newday, newHours):
+        self.course = newCourse
+        self.day = newDay
+        self.hours = newHours
+
+    
