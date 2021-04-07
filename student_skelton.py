02e24648e0854e7f182b6d6936184b5caa818ba5
Mary831 <82053519+Mary831@users.noreply.github.com>
2021-04-07 17:08:01 -0500

    Update student_skelton.py

--- student_skelton.py
+++ student_skelton.py
@@ -4,7 +4,7 @@
 # note the name of this file is student_skeleton.py not student.py which means it wont work
 # with the current version1.py program cause that one imports student not student_skeleton
 
-import sys
+
 import os
 #import version1
 
@@ -31,6 +31,8 @@ def stuff():   #note change this name
             emrem()
         elif cmd == "psr":
             psr()
+        elif cmd == "--help":
+            gethelp()
         elif cmd != "exit":
             print("invalid command")
 
@@ -62,3 +64,14 @@ def emrem():
 
 def psr():
     print("Password has been reset")
+    
+def gethelp():
+    print("regact\tRegister account with given email and password\n")
+    print("edact\tEdit account details\n")
+    print("vtut\tView list of tutors by given date and course number\n")
+    print("aapt\tAdd appointment by a given date, tutor, and course number\n")
+    print("edapt\tEdit current appointment with new date, tutor, or course number\n")
+    print("dapt\tDelete appointment by a given date, tutor, and course number\n")
+    print("emcon\tSend an email confirmation of appointment to student email provided a given date\n")
+    print("emrem\tSend an email reminder of appointmentto student email provided a given date\n")
+    print("psr\tPassword reset that takes old and then new password\n")
