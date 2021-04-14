70b7b839f81c414171a2b470dfb00ebac680cf25
divy6872 <64673254+divy6872@users.noreply.github.com>
2021-04-13 21:18:19 -0500

    Update faculty.py

--- faculty.py
+++ faculty.py
@@ -3,7 +3,7 @@ from user import User
 class Faculty(User):
 
     def rnrpt():
-        print("Run the report of your choosing")
+        print(" Run the report of your choosing")
 
     def srpt():
         print("Schedule a report to be emailed periodically")
