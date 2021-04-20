d6e913c8ef76b2deb017391e812cd6a4c5322065
Ryan Gann <43328613+RyanGann@users.noreply.github.com>
2021-04-20 13:39:18 -0500

    server things

--- server_stuff.py
+++ server_stuff.py
@@ -2,10 +2,10 @@
 # Use this documentation as a guide: 
 # https://realpython.com/python-requests/#getting-started-with-requests
 
-# remember to "pip install requests"
-
 import requests
+import sys
 
+'''
 response = requests.get('http://quanthu.life:8000/users')
 
 if response.status_code == 200:
@@ -15,4 +15,11 @@ elif response.status_code == 404:
 
 #Can do .json() instead of .text to store the data in a dictionary that can be
 #acessed by key.
+
 print(response.text)
+posts = requests.post('http://quanthu.life:8000/users', data={"_id":"1222", "username": "ryan", "email": "rgann2@una.edu","password":"12345" ,"phone":"2054956944","role":"student","lastActivityDateTime":"04/20/2021","isActive":true})
+print(posts.text)
+'''
+sys('curl -X GET "http://quanthu.life:8000/users" -H  "accept: application/json')
+sys('curl -X POST "http://quanthu.life:8000/users" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"_id\": \"12345\",\"username\":\"rgann\",\"email\":\"rgann2@una.edu\",\"password\":\"Ham1234!\",\"phone\":\"2054956944\",\"role\":\"admin\",\"lastActivityDateTime\":\"04/20/2021\",\"isActive\":true}"')
+sys('curl -X GET "http://quanthu.life:8000/users" -H  "accept: application/json')
