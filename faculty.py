de4920b42f0c485d8365c08ba7a5aba3352a9d18
Mary831 <82053519+Mary831@users.noreply.github.com>
2021-04-27 12:53:19 -0500

    Update faculty.py

--- faculty.py
+++ faculty.py
@@ -7,3 +7,8 @@ class Faculty(User):
 
     def srpt():
         print("Schedule a report to be emailed periodically")
+        
+    def gethelp():
+        print("rnrpt\tRun given report\n")
+        print("srpt\tSchedule a report to be emailed periodically\n")
+        print("psr\tPassword reset that takes old and then new password\n")
