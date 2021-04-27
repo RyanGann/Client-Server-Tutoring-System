864aa82d6c6b41ddbfdbb084510a7b7288d8c7e3
Mary831 <82053519+Mary831@users.noreply.github.com>
2021-04-27 12:44:12 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@ from user import User
 
 class Admin(User):
 
-    def addad():
+    def adad():
         print("Add a new admin account")
 
     def edad():
@@ -49,3 +49,23 @@ class Admin(User):
 
     def srpt():
         print("Schedule a report to be emailed periodically")
+        
+    def gethelp():
+        print("adad\tAdd a new admin with an email, name, and password\n")
+        print("edad\tEdit an admin's email, name, or password\n")
+        print("delad\tDelete an admin's account\n")
+        print("adfac\tAdd a new faculty with an email, name, and password\n")
+        print("edfac\tEdit a faculty's email, name, or password\n")
+        print("delfac\tDelete a faculty's account\n")
+        print("adtut\tAdd a new tutor with an email, name, and password\n")
+        print("edtut\tEdit a tutor's email, name, or password\n")
+        print("deltut\tDelete a tutor's accountd\n")
+        print("adts\tAdd a new availability schedule for a given tutor\n")
+        print("edts\tEdit a given tutor's availability schedule\n")
+        print("delts\tDelete a given tutor's availability schedule\n")
+        print("psa\tPurge a given student's account\n")
+        print("papt\tPurge appointment data by given date\n")
+        print("rnrpt\tRun given report\n")
+        print("srpt\tSchedule a report to be emailed periodically\n")
+        print("psr\tPassword reset that takes old and then new password\n")
+
