0db4ca8d7bcb16b0c80bf5610dc5d83b44b40971
Mary831 <82053519+Mary831@users.noreply.github.com>
2021-05-04 11:08:56 -0500

    changed help function

--- cli.py
+++ cli.py
@@ -15,10 +15,15 @@ from utilities import clear_screen
 Help page for dashboard
 '''
 def getHelp():
-  print('\nEnter \'login\' to login')
-  print('Enter \'regact -u <portal_email>\' to register an account')
-  print('Enter \'exit\' to quit')
-  print('Enter \'clear\' to clear the screen\n')
+    infile = open("help_user.txt", "r")
+
+    lines = infile.readlines()
+
+    for line in lines:
+      print(line, end = "")
+    
+    print()
+    infile.close()
 
 
 def login():
